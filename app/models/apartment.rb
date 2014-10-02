5b13b566ae52ae54810196e52c7cae0ce9a9f707
Ezequiel Politzer <ezepolit@gmail.com>
2014-10-02 17:49:01 -0400

    Fixed Tour Image Scroll

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -4,10 +4,10 @@ class Apartment < ActiveRecord::Base
  	has_many :apartment_showings
  	has_many :showings, through: :apartment_showings
 
- 	has_attached_file :image, styles: { medium:  "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, presence: true;
+ 	has_attached_file :image, styles: { large: "640x480", medium:  "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, presence: true;
 
-  	validates :title, presence: true;
-  	validates :neighborhood, presence: true;
-  	validates :price, presence: true;
+  validates :title, presence: true;
+	validates :neighborhood, presence: true;
+	validates :price, presence: true;
 end
