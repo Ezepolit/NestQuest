5a28b868238274c346fa7579d99eb4b395a54d80
Ezequiel Politzer <ezepolit@gmail.com>
2014-10-14 16:14:14 -0400

    Added Agent Profile to Showing

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -8,6 +8,6 @@ class Broker < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	has_attached_file :avatar, :styles => { :medium => "1000x10000>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "1000x10000>", :thumb => "100x100>" }, :default_url => "/assets/avatar_2x.png"
  	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
