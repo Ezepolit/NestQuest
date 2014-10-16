b1510f77fee7bfee8591dfd9859b77de26e52d42
Ezequiel Politzer <ezepolit@gmail.com>
2014-10-16 01:28:07 -0400

    Added new design layout

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -5,7 +5,7 @@ class Apartment < ActiveRecord::Base
   has_many :apartment_photos
  	has_many :showings, through: :apartment_showings
 
- 	has_attached_file :image, styles: { large: "640x480", medium: "300", thumb: "100" }, default_url: "/images/:style/missing.png"
+ 	has_attached_file :image, styles: { large: "640x480>", medium: "300>", thumb: "100x100>" }, default_url: "http://placehold.it/100"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, presence: true;
   accepts_nested_attributes_for :apartment_photos, :reject_if => :all_blank, :allow_destroy => true
 
--- app/models/apartment_photo.rb
+++ app/models/apartment_photo.rb
@@ -1,7 +1,7 @@
 class ApartmentPhoto < ActiveRecord::Base
   belongs_to :apartment
 
-  has_attached_file :photos, styles: { large: "640x480", medium:  "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photos, styles: { large: "640x480", medium:  "300x300", small: "200x150", thumb: "100x100" }, default_url: "http://placehold.it/100x100"
   validates_attachment_content_type :photos, content_type: /\Aimage\/.*\Z/, presence: true;
 
 end
